9c403b10c3527ca6dd4b270eb33d6306cc8e1070
kisacool <kisacoolrulit@mail.ru>
2023-03-23 21:12:53 +0300

    add Sem1Task00

---
+++ Sem1Task00/Program.cs
@@ -0,0 +1,22 @@
+﻿// Считываем данные с консоли
+string? inLine = Console.ReadLine();
+
+//
+if (inLine != null)
+{
+  //
+  int inNumber = int.Parse(inLine);
+
+  //
+  int outNumber = inNumber * inNumber;
+  int outStrtPow = (int)Math.Pow(inNumber,2);
+
+  //
+
+  //
+  Console.WriteLine(outNumber);
+
+  //
+  Console.WriteLine(outStrtPow);
+}
+
