393234c1b2c7d469002d8055be3f623190d3b249
KISACOOL <kisacoolrulit@mail.ru>
2023-03-24 16:15:14 +0300

    add corrected Sem1Task00 Sem1Task001

--- Sem1Task00/Program.cs
+++ Sem1Task00/Program.cs
@@ -1,22 +1,20 @@
 ﻿// Считываем данные с консоли
 string? inLine = Console.ReadLine();
 
-//
+// Проверим чтобы данные не были пустыми
 if (inLine != null)
 {
-  //
+  // Парсим введенное число
   int inNumber = int.Parse(inLine);
 
-  //
+  // Находим квадрат числа
   int outNumber = inNumber * inNumber;
   int outStrtPow = (int)Math.Pow(inNumber,2);
 
-  //
-
-  //
+  // Выводим данные в консоль
   Console.WriteLine(outNumber);
 
-  //
+  // 
   Console.WriteLine(outStrtPow);
 }
 
