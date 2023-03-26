0a838af800a017e5d78db33db847eb334af2cba7
kisacool <kisacoolrulit@mail.ru>
2023-03-26 21:23:55 +0300

    add Sem1Task008HW

---
+++ Sem1Task008HW/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число");
+
+string? numNLine = Console.ReadLine();
+
+if (numNLine != null)
+{
+  int numN = int.Parse(numNLine);
+  int startNum = 1;
+  string resLine = string.Empty;
+  int res = int.Parse(resLine);
+
+  while (startNum <= numN)
+  {
+    if(res%2 == 0)
+    {
+      res = res + startNum + "_";
+    }
+    startNum++;
+  }
+  Console.WriteLine(res);
+}
