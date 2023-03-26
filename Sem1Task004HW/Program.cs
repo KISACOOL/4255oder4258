6f272cba214c8c8009731826bec6d85cf8513a84
kisacool <kisacoolrulit@mail.ru>
2023-03-26 19:33:48 +0300

    add Sem1Task004HW

---
+++ Sem1Task004HW/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите первое число");
+string? numLine1 = Console.ReadLine();
+
+Console.WriteLine("Введите второе число");
+string? numLine2 = Console.ReadLine();
+
+Console.WriteLine("Введите третье число");
+string? numLine3 = Console.ReadLine();
+
+if (numLine1 != null && numLine2 != null && numLine3 != null)
+{
+  int num1 = int.Parse(numLine1);
+  int num2 = int.Parse(numLine2);
+  int num3 = int.Parse(numLine3);
+  
+  int max = num1 ;
+
+    if(max < num2)
+    {
+      max = num2 ; 
+    }
+
+    if(max < num3)
+    {
+      max  = num3;
+    }
+    
+    Console.Write("max = ");
+    Console.WriteLine(max);
+}
+
+  
