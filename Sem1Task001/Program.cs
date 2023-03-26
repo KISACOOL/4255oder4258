e2382cfde66a2558723e96c73375ba80b06559bf
kisacool <kisacoolrulit@mail.ru>
2023-03-26 10:10:05 +0300

    After the meger

--- Sem1Task001/Program.cs
+++ Sem1Task001/Program.cs
@@ -5,7 +5,7 @@ if (numLine1 != null && numLine2 != null)
 {
   int num1 = int.Parse(numLine1);
   int num2 = int.Parse(numLine2);
-}
+
 if(num1 == num2*num2)
 {
 Console.WriteLine("Первое число - квадрат второго числа");
