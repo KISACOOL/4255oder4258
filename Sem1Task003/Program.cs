efc953f44f7aa12bf50a10cb38c3abdee7b56ea7
kisacool <kisacoolrulit@mail.ru>
2023-03-26 12:22:14 +0300

    add Sem1Task003

---
+++ Sem1Task003/Program.cs
@@ -0,0 +1,37 @@
+﻿//Console.WriteLine("Введите день недели");
+
+//string? dayLine = Console.ReadLine();
+
+//if (dayLine != null)
+//{
+//  string[] dayWeek = new string[7];
+  
+//  dayWeek[0] = "понедельник";
+//  dayWeek[1] = "вторник";
+//  dayWeek[2] = "среда";
+//  dayWeek[3] = "четверг";
+// dayWeek[4] = "пятница";
+//  dayWeek[5] = "суббота";
+//  dayWeek[6] = "воскресенье";
+  
+//  int day = int.Parse(dayLine);
+  
+//  Console.WriteLine(dayWeek[day - 1]);
+//} 
+   
+
+
+Console.WriteLine("Введите день недели");
+//считываем данные с консоли
+string? inputLine = Console.ReadLine();
+//Проверяем, чтобы данные были не пустыми
+if (inputLine != null)
+{
+  //Парсим введенное число
+  int inputNumber = int.Parse(inputLine);
+
+  string outDayOfWeek = System.Globalization.CultureInfo.GetCultureInfo("ru-RU").DateTimeFormat.GetDayName((DayOfWeek)Enum.GetValues(typeof(DayOfWeek)).GetValue(inputNumber));
+  //Выводим данные в консоль
+  Console.WriteLine(outDayOfWeek);
+}
+
