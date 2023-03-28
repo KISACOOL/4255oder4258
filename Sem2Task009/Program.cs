91cbc54cc4c69c2545a6363bdb1b1e31d9b1c52f
KISACOOL <kisacoolrulit@mail.ru>
2023-03-28 16:57:33 +0300

    changed Sem2Task009

--- Sem2Task009/Program.cs
+++ Sem2Task009/Program.cs
@@ -10,17 +10,45 @@ int rndNum = numSintezator.Next(10,100);
 
 Console.WriteLine(rndNum);
 
-short firstNum = (short) (rndNum/10);
-short secondNum = (short) (rndNum%10);
+// можно использовать тип размера short но он меньше int (использование short грамотнее так как использует меньший обьем оперативной памяти)
+// short firstNum = (short) (rndNum/10);
+// short secondNum = (short) (rndNum%10);
+
+
+int firstNum = rndNum/10;
+int secondNum = rndNum%10;
 
 if (firstNum > secondNum)
 {
-  Console.WriteLine("первое число " + firstNum + "второго" + secondNum);
+  Console.WriteLine("первое число " + firstNum + " больше второго " + secondNum);
 }
 else
 {
-  Console.WriteLine(secondNum);
+  Console.WriteLine("второе число " + secondNum + " больше первого " + firstNum);
 }
 
 // вариант 2
+
+char[] digits = numSintezator.Next(10,100).ToString().ToCharArray();
+Console.WriteLine(digits);
+
+firstNum = (int) (digits[0]) - 48; 
+secondNum = (int) (digits[1]) - 48;
+
+// <переменная> = (условие)? <значение 1>: <значение 2>; 
+
+// if (условие)
+// {
+//   <переменная> = <значение 1>;
+// }
+// else
+// {
+//   <переменная> = <значение 2>;
+// }
+
+int res = (firstNum > secondNum)? firstNum:secondNum;
+Console.WriteLine(res);
+
+
+
 
