524fd88a3261429e7f38ae258843aeda7f578ba5
kisacool <kisacoolrulit@mail.ru>
2023-03-29 19:34:18 +0300

    add changed Sem2Task015HW

--- Sem2Task015HW/Program.cs
+++ Sem2Task015HW/Program.cs
@@ -7,45 +7,42 @@
 // 1 -> нет
 // * Сделать вариант с использованием конструкции Dictionary
 
-// Console.WriteLine("Введите номер дня");
-// int day = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine("Введите номер дня");
+int day = int.Parse(Console.ReadLine()??"0");
 
-// if (day < 1 || day > 7)
+if (day < 1 || day > 7)
 
 // // Во время семинара на доске, при решении этой задачи
 // // была допущена ошибка, а именно вместо знака "||" (или)
 // // был поставлен "&&" (и), но day не может быть и меньше одного
 // // и больше семи одновременно))) 
 
-// {
-//   Console.WriteLine("это не день недели");
-// }
-// else
-// {
-//   if(day >= 1 && day <= 5)
-//   {
-//     Console.WriteLine("это рабочий день"); 
-//   }
-//   else
-//   {
-//     Console.WriteLine("это выходной день");
-//   }
-// } 
+{
+  Console.WriteLine("это не день недели");
+}
+else
+{
+  if(day >= 1 && day <= 5)
+  {
+    Console.WriteLine("это рабочий день"); 
+  }
+  else
+  {
+    Console.WriteLine("это выходной день");
+  }
+} 
 
 // Вариант 2*
 
-Console.WriteLine("Введите число");
-int num = int.Parse(Console.ReadLine()??"0");
+// Dictionary<int,string> week = new Dictionary<int,string>
+// { 
+//   [1] = "monday",
+//   [2] = "tuesday",
+//   [3] = "wednesday",
+//   [4] = "Thursday",
+//   [5] = "friday",
+//   [6] = "saturday",
+//   [7] = "sunday"
+// };
 
-var dayWeek = new Dictionary<int,string>()
-{ 
-  {1, "monday"},
-  {2, "tuesday"},
-  {3, "wednesday"},
-  {4, "Thursday"},
-  {5, "friday"},
-  {6, "saturday"},
-  {7, "sunday"}
-};
 
-num = dayWeek
