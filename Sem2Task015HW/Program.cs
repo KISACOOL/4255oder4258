81a98ce0b03201d0dfa27cb0fcf97673640994b2
kisacool <kisacoolrulit@mail.ru>
2023-03-30 19:29:37 +0300

    add new Sem2Task015HW

--- Sem2Task015HW/Program.cs
+++ Sem2Task015HW/Program.cs
@@ -7,42 +7,64 @@
 // 1 -> нет
 // * Сделать вариант с использованием конструкции Dictionary
 
-Console.WriteLine("Введите номер дня");
-int day = int.Parse(Console.ReadLine()??"0");
+// Console.WriteLine("Введите номер дня");
+// int day = int.Parse(Console.ReadLine()??"0");
 
-if (day < 1 || day > 7)
+// if (day < 1 || day > 7)
 
-// // Во время семинара на доске, при решении этой задачи
-// // была допущена ошибка, а именно вместо знака "||" (или)
-// // был поставлен "&&" (и), но day не может быть и меньше одного
-// // и больше семи одновременно))) 
+// // // Во время семинара на доске, при решении этой задачи
+// // // была допущена ошибка, а именно вместо знака "||" (или)
+// // // был поставлен "&&" (и), но day не может быть и меньше одного
+// // // и больше семи одновременно))) 
 
-{
-  Console.WriteLine("это не день недели");
-}
-else
-{
-  if(day >= 1 && day <= 5)
-  {
-    Console.WriteLine("это рабочий день"); 
-  }
-  else
-  {
-    Console.WriteLine("это выходной день");
-  }
-} 
+// {
+//   Console.WriteLine("это не день недели");
+// }
+// else
+// {
+//   if(day >= 1 && day <= 5)
+//   {
+//     Console.WriteLine("это рабочий день"); 
+//   }
+//   else
+//   {
+//     Console.WriteLine("это выходной день");
+//   }
+// } 
 
 // Вариант 2*
+Console.WriteLine("Введите номер дня");
+int day = int.Parse(Console.ReadLine()??"0");
+void Week(int args)
+{
+Dictionary<int,string> WeekList = new Dictionary<int,string>();
 
-// Dictionary<int,string> week = new Dictionary<int,string>
-// { 
-//   [1] = "monday",
-//   [2] = "tuesday",
-//   [3] = "wednesday",
-//   [4] = "Thursday",
-//   [5] = "friday",
-//   [6] = "saturday",
-//   [7] = "sunday"
-// };
+  WeekList.Add(1, "monday");
+  WeekList.Add(2, "tuesday");
+  WeekList.Add(3, "wednesday");
+  WeekList.Add(4, "Thursday");
+  WeekList.Add(5, "friday");
+  WeekList.Add(6, "saturday");
+  WeekList.Add(7, "sunday");
+  string value = "";
 
+Console.WriteLine(value);
+}
+ 
+Week(day);
 
+// static void Main(string[] args)
+//         {
+//             Dictionary<string, string> AuthorList = new Dictionary<string, string>();
+ 
+//             AuthorList.Add("ggt", "гистидин");
+//             AuthorList.Add("ggg", "фенилаланин");
+//             AuthorList.Add("ctg", "глицин");
+//             AuthorList.Add("ttt", "глицин");
+//             string value = "";
+//             if (AuthorList.TryGetValue("ttt", out value))
+//             {
+//           Console.WriteLine(value);      
+//             }
+//             Console.ReadKey();
+//         }
