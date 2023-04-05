79c304679eb6be5d4c6c9048d6a433b2fba5467d
kisacool <kisacoolrulit@mail.ru>
2023-04-05 20:33:10 +0300

    add Sem4Task025HW

---
+++ Sem4Task025HW/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача №25
+// Напишите цикл, который принимает на вход два числа
+// (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+// * Написать калькулятор с операциями +, -, /, * и возведение в степень
+
+//Метод чтения консоли
+int ReadData(string msg)
+{
+  Console.WriteLine(msg);
+  int res = int.Parse(Console.ReadLine() ?? "0");
+  return res;
+}
+
+//Метод вывода результатов
+void PrintResult(string msg, int res)
+{
+  Console.WriteLine(msg + res);
+}
+
+long MyPow(int a, int b)
+{
+  long res = 1;
+
+  return res;
+}
