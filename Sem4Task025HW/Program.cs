e7a0057ef002dbf037dd3eef5d3bace3b39b81c0
KISACOOL <kisacoolrulit@mail.ru>
2023-04-06 16:47:42 +0300

    add Task* Sem4Task025HW

--- Sem4Task025HW/Program.cs
+++ Sem4Task025HW/Program.cs
@@ -5,7 +5,84 @@
 // 2, 4 -> 16
 // * Написать калькулятор с операциями +, -, /, * и возведение в степень
 
-//Метод чтения консоли
+// //Метод чтения консоли
+// int ReadData(string msg)
+// {
+//   Console.WriteLine(msg);
+//   int res = int.Parse(Console.ReadLine() ?? "0");
+//   return res;
+// }
+
+// //Метод вывода результатов
+// void PrintResult(string msg, long res)
+// {
+//   Console.WriteLine(msg + res);
+// }
+
+// //Метод возведения в стпень 
+// long MyPow(int a, int b)
+// {
+//   long res = 1;
+//   while(b > 0)
+//   {
+//     res = res * a;
+//     b = b - 1; 
+//   }
+//   return res;
+// }
+
+// int a = ReadData("Введите число А: ");
+// int b = ReadData("Введите число В: ");
+// long numPow = MyPow(a,b);
+// PrintResult("Число А в степени В равно: ", numPow);
+
+// ///////////////////////////////////////////////////////////////
+
+// // Это метод Арада учимся работать в команде)))
+
+// int ReadNum1(string message)
+// {
+// Console.WriteLine(message);
+// int num1 = int.Parse(Console.ReadLine() ?? "0");
+// return num1;
+// }
+
+// // Вот это вообще зачем нужно?
+
+// // int ReadNum2(string message)
+// // {
+// // Console.WriteLine(message);
+// // int num2 = int.Parse(Console.ReadLine() ?? "0");
+// // return num2;
+// // }
+
+// void PrintResult(string message, int res) // я бы поменял на long res
+// {
+// Console.WriteLine(message+res);
+// }
+
+// int CountDigit(int Number1, int Number2)
+// {
+// int res2 = 1;    // long
+// int result = 1;  // long
+// while(result<=Number2)
+// {
+// result=result +1;
+// res2 = Number1*res2;
+
+// }
+
+// return res2 ;
+// }
+// int numberA = ReadNum1("put number 1: ");
+// int numberB = ReadNum1("put number 2: ");
+// int res2 = CountDigit (numberA, numberB); //long
+// PrintResult ("CountDigit: ",(int)res2);  // (int) нафиг
+
+//////////////////////////////////////////////////////////////
+
+// Задача №25* Калькулятор
+
 int ReadData(string msg)
 {
   Console.WriteLine(msg);
@@ -13,15 +90,41 @@ int ReadData(string msg)
   return res;
 }
 
-//Метод вывода результатов
-void PrintResult(string msg, int res)
+int ReadDataOper (string msg)
 {
-  Console.WriteLine(msg + res);
-}
-
-long MyPow(int a, int b)
-{
-  long res = 1;
-
+  Console.WriteLine(msg);
+  char res = Convert.ToChar(Console.ReadLine());
   return res;
 }
+
+long Sum(int a, int b)
+{
+  long res = a + b;
+  return res;
+}
+
+long Sub(int a, int b)
+{
+  long res = a - b;
+  return res;
+}
+
+double Div(int a, int b)
+{
+  double res = a / b;
+  return res;
+}
+
+long Mult(int a, int b)
+{
+  long res = a * b;
+  return res;
+}
+
+int a = ReadData("Введите число А: ");
+int oper = ReadDataOper("Введите знак операции: ");
+int b = ReadData("Введите число В: ");
+if (oper == '+' ) Console.WriteLine(Sum(a,b));
+if (oper == '-' ) Console.WriteLine(Sub(a,b));
+if (oper == '/' ) Console.WriteLine(Div(a,b));
+if (oper == '*' ) Console.WriteLine(Mult(a,b));
